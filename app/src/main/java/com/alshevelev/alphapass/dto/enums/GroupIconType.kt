29af4b9704dd8a475b20337f8c87ab82c178fd0e
Alexander Shevelev <alexander.shevelev@ivcs.su>
2018-03-20 09:02:37 +0300

    Add set of icons

---
+++ app/src/main/java/com/alshevelev/alphapass/dto/enums/GroupIconType.kt
@@ -0,0 +1,10 @@
+package com.alshevelev.alphapass.dto.enums
+
+/** */
+enum class GroupIconType(val order: Int) {
+    ONLINE_SERVICE(1),
+    SOCIAL_NETWORK(2),
+    MESSENGER(3),
+    ONLINE_PAYMENT_SOLUTION(4),
+    ACTIVITY(5)
+}
